cf6523b987714c6ad6a97e31d3d6cf37916ad583
Pritam Pratik Agrawal <pritamagrawal95@gmail.com>
2020-06-14 14:37:22 +0530

    Designing Submit Secrets and tweaking app.js

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String            //to save the id of user registration via google accounts to prevent re-creation of subsequent data in database for same user
+    googleId: String,            //to save the id of user registration via google accounts to prevent re-creation of subsequent data in database for same user
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -83,11 +84,37 @@ app.get("/register",function(req,res){
     res.render("register");
 });
 app.get("/secrets",function(req,res){
+    User.find({"secret": {$ne:null}}, function(err,foundUsers){
+        if(err){
+            console.log(err);            
+        }else {
+            if(foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
+});
+app.get("/submit",function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
+});
+app.post("/submit",function(req,res){
+    const submittedSecret = req.body.secret;
+    User.findById(req.user.id, function(err, foundUser){
+        if(err){
+            console.log(err);            
+        }else {
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
 });
 app.get("/logout",function(req,res){
     req.logout();
